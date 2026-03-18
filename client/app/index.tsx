fdd4d72eb10e5ecf727f956f76074b0940f79730
Mario Gil Correa <mario.gil-correa@code.org>
2026-03-18 11:40:23 -0400

    Fetching stations data for dropdown

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { useEffect, useState  } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import Constants from "expo-constants";
+import StationsDropdown from "./stations-dropdown";
+
+const API_URL = Constants.expoConfig?.extra?.apiUrl ?? "http://localhost:5001";
 
 export default function Index() {
+  const [stationId, setStationId] = useState('');
+  const [stations, setStations] = useState([]);
+
+  useEffect(() => {
+  fetch(`${API_URL}/api/stations`)
+    .then(res => res.json())
+    .then(setStations);
+}, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <StationsDropdown 
+          stations={stations}
+          stationId={stationId}
+          setStationId={setStationId}
+        />
+        <Text>{"Selected station ID: " + stationId}</Text>
+      
+      </View>      
   );
 }

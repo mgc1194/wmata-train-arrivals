d40dd2a8fff492fe5e754ab62d5e70848d1ce668
Mario Gil Correa <mario.gil-correa@code.org>
2026-03-18 21:25:36 -0400

    Refactoring fetch error handling, displaying errors, and periodically re-fetch arrival data

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,20 +1,81 @@
 import { useEffect, useState  } from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { Text } from "react-native-paper";
 import Constants from "expo-constants";
 import StationsDropdown from "./stations-dropdown";
+import NextTrainsTable, {TrainArrival} from "./next-trains-table";
+import theme from "./theme";
 
 const API_URL = Constants.expoConfig?.extra?.apiUrl ?? "http://localhost:5001";
 
+interface Station {
+  name: string;
+  code: string[];
+}
+
+interface StationOptions {
+  itemAccessibilityLabelField: string;
+  label: string;
+  value: string;
+}
+
 export default function Index() {
-  const [stationId, setStationId] = useState('');
-  const [stations, setStations] = useState([]);
+  const [stations, setStations] = useState<StationOptions[]>([]);
+  const [stationId, setStationId] = useState<string | undefined>(undefined);
+  const [currentStation, setCurrentStation] = useState<Station | undefined>(undefined);
+  const [trainArrivals, setTrainArrivals] = useState<Record<string, TrainArrival[]>>({});
+  const [error, setError] = useState<string | null>(null);
+
+  // We are handleing multiple fetch requests in this component, we want to have a consistent 
+  // error handling strategy across all of them. It takes a promise that resolves to a Response 
+  // object and an error message to use if the fetch fails. It returns a promise that resolves 
+  // to the parsed JSON data if the fetch is successful, or sets the error state if it fails.
+  const handleFetch = (promise: Promise<Response>, errorMsg: string) => 
+    promise
+      .then(response => {
+        if (!response.ok) throw new Error(`${errorMsg}: ${response.statusText}`);
+        return response.json();
+      })
+      .catch(err => {
+        setError(err.message ?? errorMsg);
+        console.error(err);
+      }
+    );
+  // This handler is used to clear any existing errors when the user selects a new station. 
+  // This way, if there was an error fetching data for the previous station, it won't be displayed 
+  // when the user selects a new station and we start fetching data for it. The error state will 
+  // only be updated if there is an error during the fetch for the new station.
+  const handleStationChange = (id: string) => {
+    setError(null);
+    setStationId(id);
+  };
 
   useEffect(() => {
-  fetch(`${API_URL}/api/stations`)
-    .then(res => res.json())
-    .then(setStations);
-}, []);
+    handleFetch(fetch(`${API_URL}/api/stations`), "Failed to fetch stations")
+      .then(data => data && setStations(data));
+  }, []);
+
+  useEffect(() => {
+    if (stationId) {
+      handleFetch(fetch(`${API_URL}/api/stations/${stationId}`), "Failed to fetch station details")
+        .then(data => data && setCurrentStation(data));
+    }
+  }, [stationId]);
+
+  useEffect(() => {
+    if (currentStation) {
+      const  fetchArrivals = () => {
+        handleFetch(fetch(`${API_URL}/api/arrivals/${currentStation.code.join(',')}`), "Failed to fetch train arrivals")
+          .then(data => data && setTrainArrivals(data))
+      };
+      // Update every 10 seconds to keep the data fresh. 
+      // The WMATA API updates every 10-20 seconds, so this should be sufficient to provide near real-time updates 
+      // without overwhelming the API with requests.
+      fetchArrivals();
+      const interval = setInterval(fetchArrivals, 10000);
+      return () => clearInterval(interval)
+    }
+  }, [currentStation]);
 
   return (
       <View
@@ -27,10 +88,31 @@ export default function Index() {
         <StationsDropdown 
           stations={stations}
           stationId={stationId}
-          setStationId={setStationId}
+          setStationId={handleStationChange}
         />
-        <Text>{"Selected station ID: " + stationId}</Text>
-      
+        {error && (
+          <Text variant="bodySmall" style={{ color: theme.colors.error, marginTop: 8 }}>
+            {error}
+          </Text>
+        )}
+        {!stationId ? (
+          <Text variant="bodyMedium" style={{ marginTop: 16 }}>
+            Please select a station to see the next train arrivals.
+          </Text>
+        ) : (
+          <ScrollView style={{ flex: 1, width: '90%', marginTop: 16 }}>
+            <Text variant="titleMedium" style={{ marginTop: 16, alignSelf: "flex-start", marginLeft: 16 }}>
+              {"Next trains arriving to " + currentStation?.name}
+            </Text>
+            {Object.entries(trainArrivals).map(
+              ([group, trains]) => (
+                <View key={group} style={{ marginTop: 16 }}>
+                  <NextTrainsTable trains={trains} />
+                </View>
+              )
+            )}
+          </ScrollView>
+        )}
       </View>      
   );
 }
